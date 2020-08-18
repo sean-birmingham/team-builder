c22282d2f43fbf35f4f2a96f18249cdf723146ef
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-18 15:04:00 -0400

    setup state for team members, displaying a member

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { teamData } from './teamData.js';
+import Member from './components/Member';
 import './App.css';
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState(teamData);
+
   return (
     <div className='App'>
       <h1>Team Builder</h1>
+      <Member members={teamMembers} />
     </div>
   );
 }
